c8d430843bf497265f739a3a05cb4a464613e4fd
Ethan Buenting <BUENTING5@iccc.cc.ia.us>
2025-05-02 14:44:28 -0500

    v2 of trying to fix WebConfig

--- src/main/java/cyberquest/config/WebConfig.java
+++ src/main/java/cyberquest/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://ethan-buenting.github.io")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOriginPatterns("https://ethan-buenting.github.io")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
